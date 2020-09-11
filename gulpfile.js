4c86bcb852e15ce1ad68da568fc201892106e57f
jDan735 <daniel734@bk.ru>
2020-09-11 18:40:58 +0300

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-const { src, dest, parallel } = require('gulp')
+const { src, dest, parallel, task } = require('gulp')
 
 const pug = require("gulp-pug")
 const sass = require("gulp-sass")
 const watch = require("gulp-watch")
-const csscomb = require("gulp-csscomb")
+// const csscomb = require("gulp-csscomb")
+
+/* Watchers */
 
 function html(){
     return watch('./pug/*.pug', () => {
         return src('./pug/*.pug')
-               .pipe(pug({pretty: true}))
+               .pipe(pug())
                .pipe(dest('./'))
     })
 }
@@ -17,9 +19,21 @@ function css(){
     return watch('./sass/*.sass', () => {
         return src('./sass/*.sass')
                .pipe(sass())
-               .pipe(csscomb())
                .pipe(dest('./css'))
     })
 }
+
+task("sass", () => {
+    return src('./sass/*.sass')
+           .pipe(sass())
+//           .pipe(csscomb())
+           .pipe(dest('./css'))
+})
+
+task("pug", () => {
+    return src('./pug/*.pug')
+           .pipe(pug())
+           .pipe(dest('./'))
+})
 
 exports.default = parallel(html, css)
