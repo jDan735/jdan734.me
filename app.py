fa93777b78453f9f0a68227899209f10927337d3
jDan735 <daniel734@bk.ru>
2020-09-30 19:18:47 +0300

    Add ftp link to home

--- app.py
+++ app.py
@@ -140,12 +140,12 @@ def ftp(path=""):
         a += f"<li><a href='{fp}{s}{b}'>/{b}<a></li>"
 
     if path == "":
-        h1 = "/ftp"
+        h2 = "/ftp"
 
     else:
-        h1 = fp.replace(f"{s}static", "").replace(s, "/")
+        h2 = fp.replace(f"{s}static", "").replace(s, "/")
 
-    return f'<!DOCTYPE html><html><head><link rel="icon" type="image/png" href="/static/favicon-16x16.png" sizes="16x16"><link rel="icon" type="image/png" href="/static/favicon-32x32.png" sizes="32x32"><link rel="icon" type="image/png" href="/static/favicon-96x96.png" sizes="96x96"><link rel="stylesheet" href="/static/css/style.css?v=2.4.8"><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>ftp</title></head><body><h1>Index of {h1}</h1><ul>' + a + "</ul></body></html>"
+    return f'<!DOCTYPE html><html><head><link rel="icon" type="image/png" href="/static/favicon-16x16.png" sizes="16x16"><link rel="icon" type="image/png" href="/static/favicon-32x32.png" sizes="32x32"><link rel="icon" type="image/png" href="/static/favicon-96x96.png" sizes="96x96"><link rel="stylesheet" href="/static/css/style.css?v=2.4.8"><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>ftp</title></head><body><h2 style="margin-top:0;">Index of {h2}</h2><ul>' + a + "</ul></body></html>"
 
 
 @app.errorhandler(404)
