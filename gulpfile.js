ba79935ec042e193cab6f6c694d48b2cfc2ac414
jDan735 <daniel734@bk.ru>
2021-01-16 11:57:00 +0300

    Enable minify for css

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ const { src, dest, parallel } = require('gulp')
 
 const sass = require("gulp-sass")
 const watch = require("gulp-watch")
+const cssmin = require("gulp-cssmin")
 
 function css(){
     return src('./scss/style.scss')
            .pipe(sass())
+           .pipe(cssmin())
            .pipe(dest('./static/css'))
 }
 
