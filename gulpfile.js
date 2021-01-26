67459987c2bf771558abd2aee0ba7454929cd370
jDan735 <daniel734@bk.ru>
2021-01-27 01:56:42 +0300

    Add gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ const { src, dest, parallel } = require('gulp')
 const sass = require("gulp-sass")
 const watch = require("gulp-watch")
 const cssmin = require("gulp-cssmin")
+const prefixer = require("gulp-autoprefixer")
 
 function css(){
     return src('./scss/style.scss')
            .pipe(sass())
+		   .pipe(prefixer())
            .pipe(cssmin())
            .pipe(dest('./static/css'))
 }
