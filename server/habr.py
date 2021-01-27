8afbfcb8a8245a383b589a78a0e927b8a0df844c
jDan735 <daniel734@bk.ru>
2021-01-27 20:56:14 +0300

    Rewrite habr to template

--- server/habr.py
+++ server/habr.py
@@ -1,13 +1,21 @@
 from .error import app
 from bs4 import BeautifulSoup
+from flask import render_template, Markup
 
 import requests
 
 
+# @app.route("/habr/<int:id_>")
+# def habr(id_):
+#     a = habr_page(id_)
+#     return '<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" /><link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" /><link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" /><link rel="stylesheet" href="/css/style.css?v=2.8.9" /><meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><body class="index habr">' + a[0] + a[1] + "</body>"
+
 @app.route("/habr/<int:id_>")
 def habr(id_):
     a = habr_page(id_)
-    return '<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" /><link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" /><link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" /><link rel="stylesheet" href="/css/style.css?v=2.8.9" /><meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><body class="index habr">' + a[0] + a[1] + "</body>"
+    return render_template("habr.html",
+                           title=a[0],
+                           content=Markup(a[1]))
 
 
 def habr_page(id_):
@@ -15,6 +23,12 @@ def habr_page(id_):
     soup = BeautifulSoup(r.text, "lxml")
     page = []
 
-    page.append("<h1 class=header>" + soup.find("h1").span.text + "</h1>")
-    page.append(str(soup.findAll("div", {"id": "post-content-body"})[0]))
+    page.append(soup.find("h1").span.text)
+
+    content = soup.findAll("div", {"id": "post-content-body"})[0]
+
+    for tag in content.findAll("code"):
+        tag["class"] = "hljs"
+
+    page.append(str(content))
     return page
