ae1dcd50bb36a3414f3469191bc69188a40bab6e
jDan735 <daniel734@bk.ru>
2021-06-16 17:44:35 +0300

    Fix 404

--- build.py
+++ build.py
@@ -1,12 +1,15 @@
 from jinja2 import Template, Environment, FileSystemLoader
 from os import listdir
 
-print(listdir("templates"))
+# import htmlmin
 
 for name in listdir("templates"):
     env = Environment(loader=FileSystemLoader("templates"))
     template = env.get_template(name)
 
     with open(name, "w", encoding="UTF-8") as file:
-        file.write(template.render())
-    # print(template.render())
+        # html = htmlmin.minify(template.render(),
+        #                       remove_empty_space=True,
+        #                       remove_comments=True)
+        html = template.render()
+        file.write(html)
